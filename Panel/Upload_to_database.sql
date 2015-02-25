2f8c17581f8e43d095bf3f3f6a615ae51fc45d43
zettabithf <zettabithf@gmail.com>
2015-02-25 17:08:26 -0500

    Added SQL File
    Since I've noticed people attempting to install the panel now, without
    an SQL file, they're running into errors. This is the file you insert
    into the database you've created.

---
+++ Panel/Upload_to_database.sql
@@ -0,0 +1,94 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bots`
+--
+
+CREATE TABLE IF NOT EXISTS `bots` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `bothwid` varchar(100) NOT NULL,
+  `ipaddress` varchar(75) NOT NULL,
+  `country` int(5) NOT NULL,
+  `installdate` int(50) NOT NULL,
+  `lastresponse` int(50) NOT NULL,
+  `currenttask` int(255) NOT NULL,
+  `operatingsys` varchar(300) NOT NULL,
+  `botversion` varchar(30) NOT NULL,
+  `privileges` varchar(5) NOT NULL,
+  `installationpath` varchar(1000) NOT NULL,
+  `computername` varchar(300) NOT NULL,
+  `lastreboot` varchar(300) NOT NULL,
+  `mark` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `plogs`
+--
+
+CREATE TABLE IF NOT EXISTS `plogs` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `ipaddress` varchar(75) NOT NULL,
+  `action` text NOT NULL,
+  `date` int(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `knock` int(10) NOT NULL,
+  `dead` int(10) NOT NULL,
+  `gate_status` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `knock`, `dead`, `gate_status`) VALUES
+(1, 5, 7, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `privileges` varchar(300) NOT NULL,
+  `status` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `privileges`, `status`) VALUES
+(1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin', 1);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
